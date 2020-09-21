1c790821801ee514448dd2abc174676e5705933a
Rasim Labibov <postrediori@gmail.com>
2020-09-21 22:33:36 +0300

    Add water to mini map

--- src/HeightmapRenderLib/MinimapRenderer.cpp
+++ src/HeightmapRenderLib/MinimapRenderer.cpp
@@ -4,6 +4,8 @@
 #include "IHeightmapRenderer.h"
 #include "MinimapRenderer.h"
 
+static const uint32_t WaterColor = 0x007F7FFF;
+
 MinimapRenderer::MinimapRenderer(VossHeightmap* map, Image* image)
     : m_map(map) {
     setImage(image);
@@ -12,11 +14,14 @@ MinimapRenderer::MinimapRenderer(VossHeightmap* map, Image* image)
 void MinimapRenderer::draw() {
     for (size_t row = 0; row < m_image->GetHeight(); row++) {
         for (size_t col = 0; col < m_image->GetWidth(); col++) {
+            uint32_t color = 0;
             int height = int(double(m_map->GetHeight(col, row)) / m_map->GetSlopeSeek() * 256.);
             if (height < 0) {
-                height = 0;
+                color = WaterColor;
             }
-            uint32_t color = (height << 16) + (height << 8) + height;
+            else {
+                color = (height << 16) + (height << 8) + height;
+            }
             m_image->putPixel(col, row, color);
         }
     }
